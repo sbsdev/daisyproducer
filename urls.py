79e91521013a256435d5f08e6b393aee54eb8073
Christian Egli <christian.egli@sbszh.ch>
2009-03-24 12:11:10 +0100

    Add a bit of styling
    - Make templates hierarchical
    - Add css

--- urls.py
+++ urls.py
@@ -8,8 +8,9 @@ urlpatterns = patterns('',
     (r'^documents/$', 'daisyproducer.documents.views.index'),
     (r'^documents/(?P<document_id>\d+)/$', 'daisyproducer.documents.views.detail'),
     (r'^documents/(?P<document_id>\d+).pdf$', 'daisyproducer.documents.views.as_pdf'),
-    # Example:
-    # (r'^daisyproducer/', include('daisyproducer.foo.urls')),
+
+    # static files
+    (r'^stylesheets/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/eglic/src/daisyproducer/media'}),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
