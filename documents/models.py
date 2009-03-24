6deac6aeafd67286aa93ea2ab56cee9742c86a89
Christian Egli <christian.egli@sbszh.ch>
2009-03-24 13:44:24 +0100

    Move the large print profile and the respective form to models

--- documents/models.py
+++ documents/models.py
@@ -20,15 +20,56 @@ BRAILLE_CONTRACTION_GRADE_CHOICES = (
 )
 
 class BrailleProfile(models.Model):
-    cellsPerLine = models.PositiveSmallIntegerField(default=40)
-    linesPerPage = models.PositiveSmallIntegerField(default=28)
+    cellsPerLine = models.PositiveSmallIntegerField("Cells per Line", default=40, max_length=4)
+    linesPerPage = models.PositiveSmallIntegerField("Line per Page", default=28, max_length=4)
     contraction = models.PositiveSmallIntegerField(default=0, choices=BRAILLE_CONTRACTION_GRADE_CHOICES)
     hyphenation = models.BooleanField(default=True)
-    showOriginalPageNumbers = models.BooleanField(default=True)
-    enableCapitalization = models.BooleanField()
-    detailedAccentedCharacters = models.BooleanField()
+    showOriginalPageNumbers = models.BooleanField("Show original page numbers", default=True)
+    enableCapitalization = models.BooleanField("Enable Capitalization")
+    detailedAccentedCharacters = models.BooleanField("Detailed Accented Characters")
 
 class BrailleProfileForm(ModelForm):
     class Meta:
         model = BrailleProfile
+
+FONTSIZE_CHOICES = (
+    ('12pt', '12pt'),
+    ('14pt', '14pt'),
+    ('17pt', '17pt'),
+    ('20pt', '20pt'),
+)
+
+FONT_CHOICES = (
+    ('Tiresias LPfont', 'Tiresias LPfont'),
+    ('LMRoman10 Regular', 'LMRoman10 Regular'),
+    ('LMSans10 Regular', 'LMSans10 Regular'),
+    ('LMTypewriter10 Regular', 'LMTypewriter10 Regular'),
+)
+
+PAGESTYLE_CHOICES = (
+    ('plain', 'Plain'),
+    ('withPageNums', 'With original page numbers'),
+    ('scientific', 'Scientific'),
+)
+
+ALIGNMENT_CHOICES = (
+    ('justified', 'justified'),
+    ('left', 'left aligned'),
+)
+
+PAPERSIZE_CHOICES = (
+    ('a3paper', 'a3paper'),
+    ('a4paper', 'a4paper'),
+)
+
+class LargePrintProfile(models.Model):
+    fontSize = models.CharField("Fontsize", default='17pt', max_length=4, choices=FONTSIZE_CHOICES)
+    font = models.CharField(default='Tiresias LPfont', max_length=60, choices=FONT_CHOICES)
+    pageStyle = models.CharField("Page style", default='plain', max_length=16, choices=PAGESTYLE_CHOICES)
+    alignment = models.CharField(default='justified', max_length=16, choices=ALIGNMENT_CHOICES)
+    paperSize = models.CharField("Papersize", default='a4paper', max_length=16, choices=PAPERSIZE_CHOICES)
+
+class LargePrintProfileForm(ModelForm):
+    class Meta:
+        model = LargePrintProfile
 
--- documents/views.py
+++ documents/views.py
@@ -1,46 +1,9 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from daisyproducer.documents.models import Document, BrailleProfileForm
+from daisyproducer.documents.models import Document, BrailleProfileForm, LargePrintProfileForm
 from django.http import HttpResponse
 from django import forms
 
 from os import system
-
-FONTSIZE_CHOICES = (
-    ('12pt', '12pt'),
-    ('14pt', '14pt'),
-    ('17pt', '17pt'),
-    ('20pt', '20pt'),
-)
-
-FONT_CHOICES = (
-    ('Tiresias LPfont', 'Tiresias LPfont'),
-    ('LMRoman10 Regular', 'LMRoman10 Regular'),
-    ('LMSans10 Regular', 'LMSans10 Regular'),
-    ('LMTypewriter10 Regular', 'LMTypewriter10 Regular'),
-)
-
-PAGESTYLE_CHOICES = (
-    ('plain', 'Plain'),
-    ('withPageNums', 'With original page numbers'),
-    ('scientific', 'Scientific'),
-)
-
-ALIGNMENT_CHOICES = (
-    ('justified', 'justified'),
-    ('left', 'left aligned'),
-)
-
-PAPERSIZE_CHOICES = (
-    ('a3paper', 'a3paper'),
-    ('a4paper', 'a4paper'),
-)
-
-class LargePrintDownloadForm(forms.Form):
-    fontSize = forms.ChoiceField(choices=FONTSIZE_CHOICES)
-    font = forms.ChoiceField(choices=FONT_CHOICES)
-    pageStyle = forms.ChoiceField(choices=PAGESTYLE_CHOICES)
-    alignment = forms.ChoiceField(choices=ALIGNMENT_CHOICES)
-    paperSize = forms.ChoiceField(choices=PAPERSIZE_CHOICES)
 
 def index(request):
     document_list = Document.objects.all()
@@ -48,17 +11,17 @@ def index(request):
 
 def detail(request, document_id):
     document = get_object_or_404(Document, pk=document_id)
-    lpform = LargePrintDownloadForm()
+    lpform = LargePrintProfileForm()
     bform = BrailleProfileForm()
     return render_to_response('documents/detail.html', locals())
 
 def as_pdf(request, document_id):
-    form = LargePrintDownloadForm(request.POST)
+    form = LargePrintProfileForm(request.POST)
 
     document = Document.objects.get(pk=document_id)
 
     response = HttpResponse(mimetype='application/pdf')
-    response['Content-Disposition'] = "attachment; filename=%s.pdf" % (document.title)
+    response['Content-Disposition'] = "attachment; filename=\"%s.pdf\"" % (document.title)
 
     if not form.is_valid():
         return HttpResponseRedirect('/documents/')
