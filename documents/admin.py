efc411b1576fff378a841acd5925bdf73d976c88
Christian Egli <christian.egli@sbszh.ch>
2009-03-24 17:07:39 +0100

    Enhance the admin display of Documents

--- documents/admin.py
+++ documents/admin.py
@@ -1,4 +1,10 @@
 from daisyproducer.documents.models import Document
 from django.contrib import admin
 
-admin.site.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+        list_display = ('title', 'author', 'publisher')
+        list_filter = ('author', 'publisher')
+        ordering = ('title',)
+        search_fields = ('title',)
+
+admin.site.register(Document,DocumentAdmin)
