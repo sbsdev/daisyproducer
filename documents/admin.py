42f789645519dc49e05c7bc9aa74537e18373f64
Christian Egli <christian.egli@sbszh.ch>
2009-03-27 15:50:38 +0100

    Add state to the admin interface.

--- documents/admin.py
+++ documents/admin.py
@@ -2,9 +2,9 @@ from daisyproducer.documents.models import Document
 from django.contrib import admin
 
 class DocumentAdmin(admin.ModelAdmin):
-        list_display = ('title', 'author', 'publisher')
-        list_filter = ('author', 'publisher')
-        ordering = ('title',)
+        list_display = ('title', 'author', 'publisher', 'state',)
+        list_filter = ('author', 'publisher', 'state',)
+        ordering = ('title', 'state',)
         search_fields = ('title',)
 
 admin.site.register(Document,DocumentAdmin)
