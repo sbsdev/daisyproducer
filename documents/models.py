b8448a1a91ba7b482065d0632e0bdd85a37998dd
Christian Egli <christian.egli@sbszh.ch>
2009-04-03 14:31:39 +0200

    Move constants into the classes where they belong.

--- documents/models.py
+++ documents/models.py
@@ -61,27 +61,29 @@ class Version(models.Model):
     class Meta:
         get_latest_by = "version_date"
 
-MIME_TYPE_CHOICES = (
-    ('application/pdf', 'Portable Document Format, PDF'),
-    ('application/msword', 'Microsoft Word files'),
-    ('application/rtf', 'Microsoft RTF files'),
-    ('text/html', 'HTML'),
-)
-
 class Attachment(models.Model):
+
+    MIME_TYPE_CHOICES = (
+        ('application/pdf', 'Portable Document Format, PDF'),
+        ('application/msword', 'Microsoft Word files'),
+        ('application/rtf', 'Microsoft RTF files'),
+        ('text/html', 'HTML'),
+        )
+    
     comment = models.CharField(max_length=255)
     mime_type = models.CharField(max_length=32, choices=MIME_TYPE_CHOICES)
     content = models.FileField(upload_to='media')
     document = models.ForeignKey(Document)
     
 # Profiles
-BRAILLE_CONTRACTION_GRADE_CHOICES = (
-    ('0', 'Grade 0'),
-    ('1', 'Grade 1'),
-    ('2', 'Grade 2'),
-)
-
 class BrailleProfile(models.Model):
+
+    BRAILLE_CONTRACTION_GRADE_CHOICES = (
+        ('0', 'Grade 0'),
+        ('1', 'Grade 1'),
+        ('2', 'Grade 2'),
+        )
+
     cellsPerLine = models.PositiveSmallIntegerField("Cells per Line", default=40, max_length=4)
     linesPerPage = models.PositiveSmallIntegerField("Line per Page", default=28, max_length=4)
     contraction = models.PositiveSmallIntegerField(default=0, choices=BRAILLE_CONTRACTION_GRADE_CHOICES)
@@ -94,37 +96,37 @@ class BrailleProfileForm(ModelForm):
     class Meta:
         model = BrailleProfile
 
-FONTSIZE_CHOICES = (
-    ('12pt', '12pt'),
-    ('14pt', '14pt'),
-    ('17pt', '17pt'),
-    ('20pt', '20pt'),
-)
-
-FONT_CHOICES = (
-    ('Tiresias LPfont', 'Tiresias LPfont'),
-    ('LMRoman10 Regular', 'LMRoman10 Regular'),
-    ('LMSans10 Regular', 'LMSans10 Regular'),
-    ('LMTypewriter10 Regular', 'LMTypewriter10 Regular'),
-)
-
-PAGESTYLE_CHOICES = (
-    ('plain', 'Plain'),
-    ('withPageNums', 'With original page numbers'),
-    ('scientific', 'Scientific'),
-)
-
-ALIGNMENT_CHOICES = (
-    ('justified', 'justified'),
-    ('left', 'left aligned'),
-)
-
-PAPERSIZE_CHOICES = (
-    ('a3paper', 'a3paper'),
-    ('a4paper', 'a4paper'),
-)
-
 class LargePrintProfile(models.Model):
+    FONTSIZE_CHOICES = (
+        ('12pt', '12pt'),
+        ('14pt', '14pt'),
+        ('17pt', '17pt'),
+        ('20pt', '20pt'),
+        )
+    
+    FONT_CHOICES = (
+        ('Tiresias LPfont', 'Tiresias LPfont'),
+        ('LMRoman10 Regular', 'LMRoman10 Regular'),
+        ('LMSans10 Regular', 'LMSans10 Regular'),
+        ('LMTypewriter10 Regular', 'LMTypewriter10 Regular'),
+        )
+    
+    PAGESTYLE_CHOICES = (
+        ('plain', 'Plain'),
+        ('withPageNums', 'With original page numbers'),
+        ('scientific', 'Scientific'),
+        )
+    
+    ALIGNMENT_CHOICES = (
+        ('justified', 'justified'),
+        ('left', 'left aligned'),
+        )
+    
+    PAPERSIZE_CHOICES = (
+        ('a3paper', 'a3paper'),
+        ('a4paper', 'a4paper'),
+        )
+    
     fontSize = models.CharField("Fontsize", default='17pt', max_length=4, choices=FONTSIZE_CHOICES)
     font = models.CharField(default='Tiresias LPfont', max_length=60, choices=FONT_CHOICES)
     pageStyle = models.CharField("Page style", default='plain', max_length=16, choices=PAGESTYLE_CHOICES)
