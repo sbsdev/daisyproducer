92e5635c03ade735155f51cda3bdb969f0bd4b00
Christian Egli <christian.egli@sbszh.ch>
2009-04-30 16:17:51 +0200

    The default-character-set doesn't seem to be a valid setting.

--- settings_prod.py
+++ settings_prod.py
@@ -4,7 +4,7 @@ DEBUG = TEMPLATE_DEBUG = False
 
 DATABASE_ENGINE = 'mysql'
 DATABASE_OPTIONS = {
-    "default-character-set": "utf8",
+#    "default-character-set": "utf8",
     "init_command": "SET storage_engine=INNODB",
 }
 DATABASE_NAME = 'daisyproducer_prod'
