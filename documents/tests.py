38394a9bba628eab4430228ee9abfef09104d052
Christian Egli <christian.egli@sbszh.ch>
2009-06-08 16:13:28 +0200

    Add more tests and enhance existing ones

--- documents/tests.py
+++ documents/tests.py
@@ -1,7 +1,8 @@
-from daisyproducer.documents.models import Document, Version
+from daisyproducer.documents.models import Document, Version, StateError
 from daisyproducer.documents.versionHelper import XMLContent
 from django.conf import settings
 from django.core.files.base import ContentFile
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 import os, shutil
 
@@ -9,25 +10,33 @@ class ManageViewTest(TestCase):
     fixtures = ['state.yaml', 'user.yaml']
 
     def setUp(self):
-        d = Document()
-        d.save()
+        self.document = Document()
+        self.document.save()
 
     def test_manage_index(self):
+        """Check if login succeeds"""
         self.client.login(username='testuser', password='foobar')
-        response = self.client.get('/manage/')
+        response = self.client.get(reverse('manage_index'))
+        self.assertTemplateUsed(response, 'documents/manage_index.html')
         self.failUnlessEqual(response.status_code, 200)
 
     def test_manage_details(self):
+        """Check if login succeeds"""
         self.client.login(username='testuser', password='foobar')
-        response = self.client.get('/manage/1/')
+        response = self.client.get(reverse('manage_detail', args=[self.document.pk]))
+        self.assertTemplateUsed(response, 'documents/manage_detail.html')
         self.failUnlessEqual(response.status_code, 200)
 
     def test_manage_index_no_user(self):
-        response = self.client.get('/manage/')
+        """Check if access w/o login fails"""
+        response = self.client.get(reverse('manage_index'))
+        self.assertTemplateNotUsed(response, 'documents/manage_index.html')
         self.failIfEqual(response.status_code, 200)
 
     def test_manage_details_no_user(self):
-        response = self.client.get('/manage/1/')
+        """Check if access w/o login fails"""
+        response = self.client.get(reverse('manage_detail', args=[self.document.pk]))
+        self.assertTemplateNotUsed(response, 'documents/manage_detail.html')
         self.failIfEqual(response.status_code, 200)
 
 class BrowseViewTest(TestCase):
@@ -53,26 +62,31 @@ class BrowseViewTest(TestCase):
         os.rename(settings.MEDIA_ROOT + '.old', settings.MEDIA_ROOT)
 
     def test_browse_index(self):
-        response = self.client.get('/')
+        """Check if access w/o login succeeds"""
+        response = self.client.get(reverse('browse_index'))
         self.failUnlessEqual(response.status_code, 200)
 
     def test_browse_details(self):
-        response = self.client.get('/%s/' % self.document.pk)
+        """Check if access w/o login succeeds"""
+        response = self.client.get(reverse('browse_detail', args=[self.document.pk]))
         self.failUnlessEqual(response.status_code, 200)
 
     def test_browse_pdf_empty(self):
-        response = self.client.post('/%s.pdf' % self.document.pk)
+        """Check if post with an empty form fails"""
+        response = self.client.post(reverse('browse_pdf', args=[self.document.pk]))
         self.failIfEqual(response.status_code, 200)
 
     def test_browse_pdf_partial(self):
-        response = self.client.post('/%s.pdf' % self.document.pk, {
+        """Check if post with an partial form fails"""
+        response = self.client.post(reverse('browse_pdf', args=[self.document.pk]), {
                 'alignment': 'justified',
                 'paperSize': 'a3paper',
                 })
         self.failIfEqual(response.status_code, 200)
 
-    def test_browse_invalid(self):
-        response = self.client.post('/%s.pdf' % self.document.pk, {
+    def test_browse_pdf_invalid(self):
+        """Check if post with an invalid form fails"""
+        response = self.client.post(reverse('browse_pdf', args=[self.document.pk]), {
                 'fontSize': 'foo', 
                 'font': 'bar',
                 'pageStyle': 'baz',
@@ -81,9 +95,9 @@ class BrowseViewTest(TestCase):
                 })
         self.failIfEqual(response.status_code, 200)
 
-    # TODO: make sure there is a version before doing this test
     def test_browse_pdf(self):
-        response = self.client.post('/%s.pdf' % self.document.pk, {
+        """Check if post with a valid form succeeds"""
+        response = self.client.post(reverse('browse_pdf', args=[self.document.pk]), {
                 'fontSize': '14pt', 
                 'font': 'Tiresias LPfont',
                 'pageStyle': 'withPageNums',
@@ -92,8 +106,35 @@ class BrowseViewTest(TestCase):
                 })
         self.failUnlessEqual(response.status_code, 200)
 
+    def test_browse_brl_empty(self):
+        """Check if post with an empty form fails"""
+        response = self.client.post(reverse('browse_brl', args=[self.document.pk]))
+        self.failIfEqual(response.status_code, 200)
+
+    def test_browse_brl_partial(self):
+        """Check if post with an partial form fails"""
+        response = self.client.post(reverse('browse_brl', args=[self.document.pk]), {
+                'cellsPerLine': '40', 
+                'linesPerPage': '28', 
+                })
+        self.failIfEqual(response.status_code, 200)
+
+    def test_browse_brl_invalid(self):
+        """Check if post with an invalid form fails"""
+        response = self.client.post(reverse('browse_brl', args=[self.document.pk]), {
+                'cellsPerLine': 'foo', 
+                'linesPerPage': 'bar', 
+                'contraction': 'baz', 
+                'hyphenation': 'not', 
+                'showOriginalPageNumbers': 'even', 
+                'enableCapitalization': 'remotely', 
+                'detailedAccentedCharacters': 'valid'
+                })
+        self.failIfEqual(response.status_code, 200)
+
     def test_browse_brl(self):
-        response = self.client.post('/%s.brl' % self.document.pk, {
+        """Check if post with a valid form succeeds"""
+        response = self.client.post(reverse('browse_brl', args=[self.document.pk]), {
                 'cellsPerLine': '40', 
                 'linesPerPage': '28', 
                 'contraction': '0', 
@@ -107,3 +148,74 @@ class BrowseViewTest(TestCase):
     # def test_login_redirect(self):
     #     response = self.client.get('/todo', follow=True)
     #     self.assertRedirects(response, '/accounts/login', status_code=301, target_status_code=200)
+
+class TodoViewTest(TestCase):
+    fixtures = ['state.yaml', 'user.yaml']
+
+    def setUp(self):
+        self.document = Document()
+        self.document.save()
+
+    def test_todo_index(self):
+        """Check if login succeeds"""
+        self.client.login(username='testuser', password='foobar')
+        response = self.client.get(reverse('todo_index'))
+        self.assertTemplateUsed(response, 'documents/todo_index.html')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_todo_details(self):
+        """Check if login succeeds"""
+        self.client.login(username='testuser', password='foobar')
+        response = self.client.get(reverse('todo_detail', args=[self.document.pk]))
+        self.assertTemplateUsed(response, 'documents/todo_detail.html')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_todo_index_no_user(self):
+        """Check if access w/o login fails"""
+        response = self.client.get(reverse('todo_index'))
+        self.assertTemplateNotUsed(response, 'documents/todo_index.html')
+        self.failIfEqual(response.status_code, 200)
+
+    def test_todo_details_no_user(self):
+        """Check if access w/o login fails"""
+        response = self.client.get(reverse('todo_detail', args=[self.document.pk]))
+        self.assertTemplateNotUsed(response, 'documents/todo_detail.html')
+        self.failIfEqual(response.status_code, 200)
+
+    def test_todo_add_attachment(self):
+        """Check if adding a valid attachment succeeds"""
+        self.client.login(username='testuser', password='foobar')
+        response = self.client.post(reverse('todo_add_attachment', args=[self.document.pk]), {
+                'comment': 'testing 123', 
+                'content': 'hah', 
+                })
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_todo_transition_get(self):
+        """Check if sending a get request to the transition url fails"""
+        self.client.login(username='testuser', password='foobar')
+        response = self.client.get(reverse('todo_transition', args=[self.document.pk]))
+        self.failIfEqual(response.status_code, 200)
+
+    def test_todo_transition_invalid_state(self):
+        """Check if transitioning to an invalid state results in rendering the form again"""
+        self.client.login(username='testuser', password='foobar')
+        response = self.client.post(reverse('todo_transition', args=[self.document.pk]), 
+                                    {'state': 'foo'})
+        self.assertTemplateUsed(response, 'documents/todo_detail.html')
+
+    def test_todo_transition_invalid_state(self):
+        """Check if transitioning to an invalid state results in rendering the form again"""
+        self.client.login(username='testuser', password='foobar')
+        response = self.client.post(reverse('todo_transition', args=[self.document.pk]), 
+                                    # 99 is certainly not a valid state
+                                    {'state': 99})
+        self.assertTemplateUsed(response, 'documents/todo_detail.html')
+
+    def test_todo_transition_invalid_state(self):
+        """Check if an invalid transition results in an StateError exception"""
+        self.client.login(username='testuser', password='foobar')
+        self.failUnlessRaises(StateError, 
+                              self.client.post,
+                              reverse('todo_transition', args=[self.document.pk]), 
+                              {'state': 3})
