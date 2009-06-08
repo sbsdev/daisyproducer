09b1830f1f8683f73ef0ad111155c8d0aa75e5e2
Christian Egli <christian.egli@sbszh.ch>
2009-06-08 11:49:49 +0200

    Disable debug for unit test and specify a test db name.

--- settings_unittest.py
+++ settings_unittest.py
@@ -1,7 +1,8 @@
 from settings_common import *
 
-DEBUG = TEMPLATE_DEBUG = True
+DEBUG = TEMPLATE_DEBUG = False
 
 DATABASE_ENGINE = 'sqlite3'
+TEST_DATABASE_NAME = 'unittest.db'
 
 DAISY_PIPELINE_PATH = os.path.join(PROJECT_DIR, '..', '..', 'tmp', 'pipeline-20090410')
