88eb8ba2bc7b213790ca28a2b7ec697ad7063caa
Christian Egli <christian.egli@sbszh.ch>
2009-06-18 10:22:42 +0200

    Add the debug toolbar to the settings

--- settings.py
+++ settings.py
@@ -8,3 +8,9 @@ DATABASE_USER = 'eglic'
 DATABASE_PASSWORD = ''
 
 DAISY_PIPELINE_PATH = os.path.join(PROJECT_DIR, '..', '..', 'tmp', 'pipeline-20090410')
+
+if DEBUG:
+    INSTALLED_APPS +=  ('debug_toolbar',)
+    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INTERNAL_IPS = ('127.0.0.1',)
+
