f9dca979768ea17cee0993dac5bac4257bda623e
Christian Egli <christian.egli@sbszh.ch>
2009-09-17 13:47:43 +0200

    Comment out the debug tool bar

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ DATABASE_PASSWORD = ''
 DAISY_PIPELINE_PATH = os.path.join(PROJECT_DIR, '..', '..', 'tmp', 'pipeline-20090410')
 
 # debug toolbar
-INSTALLED_APPS +=  ('debug_toolbar',)
-MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+#INSTALLED_APPS +=  ('debug_toolbar',)
+#MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 INTERNAL_IPS = ('127.0.0.1',)
 DEBUG_TOOLBAR_CONFIG = {'INTERCEPT_REDIRECTS' : False}
