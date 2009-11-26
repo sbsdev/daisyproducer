16839bd27cc9b2eb9cf025e47ce089c9cbe53e2d
Christian Egli <christian.egli@sbszh.ch>
2009-11-26 17:08:35 +0100

    Add a demo user and groups for a initial install
    For an initial import it would be nice to have an initial user and a
    few groups that are responsible for the predefined states. This makes
    an installation much simpler and a new user can get started fairly
    quickly.

--- documents/sql/state.sql
+++ documents/sql/state.sql
@@ -5,6 +5,7 @@ INSERT INTO documents_state (name, sort_order) VALUES ('ocred', 3);
 INSERT INTO documents_state (name, sort_order) VALUES ('marked_up', 4);
 INSERT INTO documents_state (name, sort_order) VALUES ('proof_read', 5);
 INSERT INTO documents_state (name, sort_order) VALUES ('approved', 6);
+
 -- transitions
 -- scanning
 INSERT INTO documents_state_next_states (from_state_id, to_state_id) VALUES (1, 2);
@@ -20,3 +21,36 @@ INSERT INTO documents_state_next_states (from_state_id, to_state_id) VALUES (5, 
 INSERT INTO documents_state_next_states (from_state_id, to_state_id) VALUES (5, 4);
 -- fixing_errata
 INSERT INTO documents_state_next_states (from_state_id, to_state_id) VALUES (6, 4);
+
+-- For an initial import it would be nice to have an initial user and
+-- a few groups that are responsible for above states 
+
+-- add groups
+INSERT INTO auth_group (id, name) VALUES (1, 'Scanners');
+INSERT INTO auth_group (id, name) VALUES (2, 'Markers');
+INSERT INTO auth_group (id, name) VALUES (3, 'Proof_Readers');
+INSERT INTO auth_group (id, name) VALUES (4, 'Approvers');
+
+-- add a demo user
+-- is unfortunately backend specific and therefore handled in state.*.sql
+
+-- add groups to demo user
+INSERT INTO auth_user_groups (id, user_id, group_id) VALUES (1, 2, 1);
+INSERT INTO auth_user_groups (id, user_id, group_id) VALUES (2, 2, 2);
+INSERT INTO auth_user_groups (id, user_id, group_id) VALUES (3, 2, 3);
+INSERT INTO auth_user_groups (id, user_id, group_id) VALUES (4, 2, 4);
+
+-- now make sure the groups are responsible for their state
+-- Scanners are responsible for new and scanned documents, i.e. they
+-- should do scanning and OCR
+INSERT INTO documents_state_responsible (id, state_id, group_id) VALUES (1, 1, 1);
+INSERT INTO documents_state_responsible (id, state_id, group_id) VALUES (2, 2, 1);
+-- Markers are responsible for ocred documents, i.e. they should do
+-- markup 
+INSERT INTO documents_state_responsible (id, state_id, group_id) VALUES (3, 3, 2);
+-- Proof_Readers are responsible for marked_up documents, i.e. they
+-- should do proof reading
+INSERT INTO documents_state_responsible (id, state_id, group_id) VALUES (4, 4, 3);
+-- Approvers are responsible for proof_read and approved documents
+INSERT INTO documents_state_responsible (id, state_id, group_id) VALUES (5, 5, 4);
+INSERT INTO documents_state_responsible (id, state_id, group_id) VALUES (6, 6, 4);
