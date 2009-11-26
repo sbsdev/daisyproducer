9126296214f3280847a0f542a9d2a70e352bf94d
Christian Egli <christian.egli@sbszh.ch>
2009-11-26 17:24:51 +0100

    After all there is a way how to handle user insertion cross-db

--- documents/sql/state.sql
+++ documents/sql/state.sql
@@ -32,7 +32,9 @@ INSERT INTO auth_group (id, name) VALUES (3, 'Proof_Readers');
 INSERT INTO auth_group (id, name) VALUES (4, 'Approvers');
 
 -- add a demo user
--- is unfortunately backend specific and therefore handled in state.*.sql
+INSERT INTO auth_user (id, username, first_name, last_name, email, password,
+       is_staff, is_active, is_superuser, last_login, date_joined) 
+VALUES (2, 'demo', 'Demo', 'User', 'demo.user@example.com', 'sha1$7f00f$139ae061147ffafd825cfec2951d86e787959aef', '0', '1', '0', '2009-11-26 16:23:51.949691','2009-11-26 16:23:37.074040');
 
 -- add groups to demo user
 INSERT INTO auth_user_groups (id, user_id, group_id) VALUES (1, 2, 1);
--- documents/sql/state.sqlite3.sql
+++
@@ -1,4 +0,0 @@
--- add a demo user
-INSERT INTO auth_user (id, username, first_name, last_name, email, password,
-       is_staff, is_active, is_superuser, last_login, date_joined) 
-VALUES (2, 'demo', 'Demo', 'User', 'demo.user@example.com', 'sha1$7f00f$139ae061147ffafd825cfec2951d86e787959aef', 0, 1, 0, '2009-11-26 16:23:51.949691','2009-11-26 16:23:37.074040');
