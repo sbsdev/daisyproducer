41bb92c738d78c49903b55f7157b7ffeb90608ea
Christian Egli <christian.egli@sbszh.ch>
2009-12-07 14:32:17 +0100

    make sure the git command is executed inside the git directory

--- version.py
+++ version.py
@@ -1,11 +1,15 @@
 from subprocess import Popen, PIPE
 from django.conf import settings
+import os
 
 def getVersion():
     """Try to get a version string from git, otherwise use a default"""
     command = ("git", "describe", "--tags")
     try:
+        cwd = os.getcwd()
+        os.chdir(settings.PROJECT_DIR)
         result = Popen(command, stdout=PIPE).communicate()[0]
+        os.chdir(cwd)
     except OSError:
         result = settings.PACKAGE_VERSION
     return result
