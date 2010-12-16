9d0b5a38f05b38d57da6292833978c32c71204db
Christian Egli <christian.egli@sbszh.ch>
2010-12-16 14:51:38 +0100

    Add a new deploy target to deploy to a test server

--- apache/updateDaisyProducer.sh
+++ apache/updateDaisyProducer.sh
@@ -1,12 +1,17 @@
 #! /bin/sh
 
-#  Copyright (C) 2009 Christian Egli
+# Copyright (C) 2009, 2010 by Swiss Library for the Blind, Visually Impaired and Print Disabled
 
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.
 
-INSTALL_DIR=/srv/demo.xmlp.sbszh.ch/daisyproducer
+case "$1" in
+production) DEPLOY_ENV=demo;;
+test|*) DEPLOY_ENV=test;;
+esac
+
+INSTALL_DIR=/srv/$DEPLOY_ENV.xmlp.sbszh.ch/daisyproducer
 
 cd $INSTALL_DIR
 git pull
