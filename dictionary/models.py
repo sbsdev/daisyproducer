563960b94f40b4f39553717a0e4cd864a7adebc3
Christian Egli <christian.egli@sbszh.ch>
2011-10-07 17:31:37 +0200

    The untranslated field should be unique

--- dictionary/models.py
+++ dictionary/models.py
@@ -6,17 +6,17 @@ from django.utils.translation import ugettext_lazy as _
 class Word(models.Model):
 
     WORD_TYPE_CHOICES = (
-        ('0', _('No restriction')),
-        ('1', _('Also as a name')),
-        ('2', _('Only as a name')),
-        ('3', _('Also as a place')),
-        ('4', _('Only as a place')),
-        ('5', _('Homograph default')),
-        ('6', _('Homograph alternative')),
-        ('7', _('Dialect')),
+        (0, _('No restriction')),
+        (1, _('Also as a name')),
+        (2, _('Only as a name')),
+        (3, _('Also as a place')),
+        (4, _('Only as a place')),
+        (5, _('Homograph default')),
+        (6, _('Homograph alternative')),
+        (7, _('Dialect')),
         )
 
-    untranslated = models.CharField(max_length=255)
+    untranslated = models.CharField(max_length=255, unique=True)
     grade1 = models.CharField(max_length=255)
     grade2 = models.CharField(max_length=255)
     document = models.ManyToManyField(Document, null=True, blank=True)
