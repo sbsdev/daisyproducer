71239027cba173416975a20432839b46782074bd
Christian Egli <christian.egli@sbszh.ch>
2011-10-21 11:56:00 +0200

    Add admin page for dictionary

---
+++ dictionary/admin.py
@@ -0,0 +1,10 @@
+from daisyproducer.dictionary.models import Word
+from django.contrib import admin
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('untranslated', 'grade1', 'grade2', 'type', 'isConfirmed')
+    ordering = ('untranslated',)
+    search_fields = ('untranslated',)
+
+admin.site.register(Word, WordAdmin)
+
