fc779b01d8dbb28b4e8a268561d715fba880748e
Christian Egli <christian.egli@sbszh.ch>
2011-12-08 12:08:22 +0100

    Explain the use of local settings

--- settings.py
+++ settings.py
@@ -14,6 +14,11 @@ INTERNAL_IPS = ('127.0.0.1',)
 DEBUG_TOOLBAR_CONFIG = {'INTERCEPT_REDIRECTS' : False}
 SERVE_STATIC_FILES = True
 
+# the following is an idea from https://code.djangoproject.com/wiki/SplitSettings
+# We have both local settings and common settings. They are used as follows:
+# - common settings are shared data between normal settings and unit test settings
+# - local settings are used on productive servers to keep the local
+#   settings such as db passwords, etc out of version control
 try:
     from settings_local import *
 except ImportError:
