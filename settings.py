45746b8533ea37b30ff080df47cb283ab1c25e85
Christian Egli <christian.egli@sbszh.ch>
2012-01-13 14:21:12 +0100

    The dtbook2sbsform project is now split from LiblouisSaxonExtension
    hence the name has changed to 'dtbook2sbsform'

--- settings.py
+++ settings.py
@@ -5,11 +5,11 @@ PACKAGE_VERSION = "0.5"
 DEBUG = TEMPLATE_DEBUG = True
 
 DAISY_PIPELINE_PATH = os.path.join(PROJECT_DIR, '..', '..', 'tmp', 'pipeline')
-DTBOOK2SBSFORM_PATH = os.path.join(PROJECT_DIR, '..', '..', 'tmp', 'LiblouisSaxonExtension')
+DTBOOK2SBSFORM_PATH = os.path.join(PROJECT_DIR, '..', '..', 'tmp', 'dtbook2sbsform')
 
 # debug toolbar
-#INSTALLED_APPS +=  ('debug_toolbar',)
-#MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS +=  ('debug_toolbar',)
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 INTERNAL_IPS = ('127.0.0.1',)
 DEBUG_TOOLBAR_CONFIG = {'INTERCEPT_REDIRECTS' : False}
 SERVE_STATIC_FILES = True
