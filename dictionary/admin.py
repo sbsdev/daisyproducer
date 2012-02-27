4c5ff2f0f205c60042b515a909dbfb1d1f205e0e
Christian Egli <christian.egli@sbszh.ch>
2012-02-27 12:19:51 +0100

    Adapt to new model

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -2,7 +2,7 @@ from daisyproducer.dictionary.models import Word
 from django.contrib import admin
 
 class WordAdmin(admin.ModelAdmin):
-    list_display = ('untranslated', 'grade1', 'grade2', 'type', 'isConfirmed', 'isLocal')
+    list_display = ('untranslated', 'braille', 'grade', 'type', 'isConfirmed', 'isLocal')
     ordering = ('untranslated',)
     search_fields = ('untranslated',)
 
