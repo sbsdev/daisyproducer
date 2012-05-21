dbc0fae1818436a41991f229651dd71db0a41f80
Christian Egli <christian.egli@sbs.ch>
2012-05-21 16:28:06 +0200

    Don't deploy jni based java bindings for the hyphenation anymore
    as jhyphen is now based on jna and can be deployed together with
    dtbook_hyphenator

--- apache/deploy.sh
+++ apache/deploy.sh
@@ -62,25 +62,6 @@ sudo make install"
     fi
 }
 
-function deploy_hyphen_java_bindings() {
-    local PACKAGE=`ls -rt ~/src/jhyphen/jhyphen-*.tar.gz|tail -1`
-    if is_newer_locally $PACKAGE $1 $2; then
-	echo "`basename $PACKAGE` is newer locally. Deploying it..."
-	scp $PACKAGE $1:$2
-	ssh -t $1 "
-cd $2
-# removing the old source seems to help with using user variables for make below
-rm -rf `basename $PACKAGE .tar.gz` 
-tar xzf `basename $PACKAGE`
-cd `basename $PACKAGE .tar.gz`
-./configure
-make $3
-sudo make install"
-    else
-	echo "`basename $PACKAGE` has already been deployed. Skipping it..."
-    fi
-}
-
 function deploy_pipeline() {
     local PACKAGE=`ls -rt ~/src/dmfc/dist/pipeline-[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].zip|tail -1`
     if is_newer_locally $PACKAGE $1 $2; then
@@ -103,7 +84,6 @@ case "$1" in
 	deploy_pipeline xmlp /opt
 	deploy_dtbook2sbsform xmlp /opt
 	deploy_dtbook_hyphenator xmlp /opt
-	deploy_hyphen_java_bindings xmlp ~/src "CPPFLAGS='-I/usr/lib/jvm/java-6-sun-1.6.0.26/include -I/usr/lib/jvm/java-6-sun-1.6.0.26/include/linux'"
 	deploy_braille_tables xmlp ~/src
 	restart_apache xmlp;;
 
@@ -112,7 +92,6 @@ case "$1" in
 	deploy_dtbook2sbsform xmlp-test /opt
 	deploy_dtbook_hyphenator xmlp-test /opt
 	deploy_braille_tables xmlp-test ~/src
-	deploy_hyphen_java_bindings xmlp-test ~/src "CPPFLAGS='-I/usr/lib/jvm/java-6-sun-1.6.0.26/include -I/usr/lib/jvm/java-6-sun-1.6.0.26/include/linux'"
 	restart_apache xmlp-test;;
     
     dev|*) 
