f3bc28e8f1d154245c5f156fd7624937aabb0d4d
Christian Egli <christian.egli@sbs.ch>
2012-07-06 16:42:45 +0200

    Migrate all apps

--- apache/updateDaisyProducer.sh
+++ apache/updateDaisyProducer.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# Copyright (C) 2009, 2010, 2011 by Swiss Library for the Blind, Visually Impaired and Print Disabled
+# Copyright (C) 2009, 2010, 2011, 2012 by Swiss Library for the Blind, Visually Impaired and Print Disabled
 
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -16,5 +16,5 @@ INSTALL_DIR=/srv/$DEPLOY_ENV.xmlp.sbszh.ch/daisyproducer
 cd $INSTALL_DIR
 git pull
 make locale/de/LC_MESSAGES/django.mo
-python manage.py migrate documents
+python manage.py migrate
 sudo /etc/init.d/apache2 restart
