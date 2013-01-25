14c30cb94ed1f76582249d97c205339c3e15a7cf
Christian Egli <christian.egli@sbs.ch>
2013-01-25 16:40:12 +0100

    Only check a product out when it is in the archive
    - Use a closure to fetch the data from the xml

--- erp/management/commands/importABACUS.py
+++ erp/management/commands/importABACUS.py
@@ -24,6 +24,7 @@ class Command(BaseCommand):
         if len(args) < 1:
             raise CommandError('No ABACUS Export file specified')
 
+        verbosity = int(options['verbosity'])
         self.numberOfDocuments = 0
 
         metadata = {
@@ -49,30 +50,42 @@ class Command(BaseCommand):
                 raise CommandError('ABACUS Export file "%s" not found' % file)
             except etree.XMLSyntaxError, e:
                 raise CommandError('Cannot parse ABACUS Export file "%s"' % file, e)
-            except lxml.etree.DocumentInvalid, e:
+            except etree.DocumentInvalid, e:
                 raise CommandError('ABACUS Export file "%s" is not valid' % file, e)
 
-            xpatheval = etree.XPathEvaluator(tree)
-            product_number = xpatheval("%s/artikel_nr" % root)[0].text
+            evaluator = etree.XPathEvaluator(tree)
+            get_key = make_get_key_fn(evaluator)
+
+            product_number = get_key("%s/artikel_nr" % root)
+            params = dict([(key, get_key("%s/MetaData/%s" % (root, value))) for (key, value) in metadata.items()])
+
+            if verbosity >= 1:
+                self.stdout.write('Processing "%s" [%s]... \n' % (params['title'], product_number))
+
+            daisy_producer = get_key("%s/MetaData/sbs/daisy_producer" % root)
+            if daisy_producer != "ja":
+                if verbosity >= 2:
+                    self.stdout.write('Ignoring "%s" as daisy_producer is set to "%s" \n' % (params['title'], daisy_producer))
+                continue
+
             try:
                 checkout_document(product_number)
             except ImportError, e:
-                print e
+                if verbosity >= 2:
+                    self.stdout.write('%s\n' % e)
                 continue
 
-            params = dict([(key, xpatheval("%s/MetaData/%s" % (root, value))[0].text) for (key, value) in metadata.items()])
-            production_series_number = xpatheval("%s/MetaData/sbs/rucksackNr" % root)[0].text
+            production_series_number = get_key("%s/MetaData/sbs/rucksackNr" % root)
             if production_series_number != '0':
                 params["production_series"] = Document.PRODUCTION_SERIES_CHOICES[1][0]
                 params["production_series_number"] = production_series_number
-            nodes = xpatheval("%s/MetaData/sbs/reihe" % root)
-            reihe = nodes[0].text if nodes else ''
-            if production_series_number == '0' and reihe.upper().find('SJW') != -1:
+            reihe = get_key("%s/MetaData/sbs/reihe" % root)
+            if production_series_number == '0' and reihe and reihe.upper().find('SJW') != -1:
                 params['production_series'] = Document.PRODUCTION_SERIES_CHOICES[0][0]
                 m = re.search('\d+', reihe) # extract the series number
                 if m != None:
                     params['production_series_number'] = m.group(0)
-            if xpatheval("%s/MetaData/sbs/Aufwand_A2" % root)[0].text == 'D':
+            if get_key("%s/MetaData/sbs/Aufwand_A2" % root) == 'D':
                 params["production_source"] = Document.PRODUCTION_SOURCE_CHOICES[0][0]
             document = Document(**params)
             print document
@@ -83,6 +96,13 @@ class Command(BaseCommand):
 
         self.stdout.write('Successfully added %s products.\n' % self.numberOfDocuments)
 
+
+def make_get_key_fn(evaluator):
+    def get_key(key):
+        node = evaluator(key)
+        return node[0].text if node else None
+    return get_key
+
 def checkout_document(product_number):
     url = 'http://pam02.sbszh.ch/alfresco/s/api/cmis'
     user = 'test_pam02_eglic'
@@ -90,11 +110,25 @@ def checkout_document(product_number):
     cmisClient = cmislib.CmisClient(url, user, password)
     repo = cmisClient.defaultRepository
 
+    # let's see if the product exists at all
     q = "select * from sbs:produkt where sbs:pProduktNo = '%s'" % product_number
     resultset = repo.query(q)
-    if not resultset.hasNext():
+    if not resultset:
         raise ImportError("Product %s not found" % product_number)
-    product = resultset.getNext()
+
+    archive_path = "PATH:\"/app:company_home/cm:Produktion/cm:Archiv//*\""
+    # we only need to check out products that are archived already
+    q = "select * from sbs:produkt where sbs:pProduktNo = '%s' AND CONTAINS('%s')" % (product_number, archive_path)
+    resultset = repo.query(q)
+    if not resultset:
+        # if it isn't in the archive then the product is new or still
+        # being worked on. Then we do not need to check anything out,
+        # i.e. we are done
+        if verbosity >= 2:
+            self.stdout.write('Product [%s] is not in archive path [%s], so no need to check it out "%s" \n' % (product_number, archive_path))
+        return
+
+    product = resultset[0]
 
     ticket = get_auth_ticket(user, password)
     h = httplib2.Http()
@@ -105,7 +139,7 @@ def checkout_document(product_number):
     if response.status != httplib.OK:
         tree = etree.HTML(content)
         error_message = tree.xpath("//div[@class='errorMessage']")
-        raise ImportError("Checkout of product %s failed\n%s" % (product.name, error_message[0].text))
+        raise ImportError("Checkout of product %s failed with \"%s\"" % (product.name, error_message[0].text))
 
 def get_auth_ticket(user, password):
     url = "http://pam02.sbszh.ch/alfresco/service/api/login?u=%s&pw=%s" % (user, password)
