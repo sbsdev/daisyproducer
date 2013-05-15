d0603912d5e5d49aa4ca9556f000ab04e055b01e
Christian Egli <christian.egli@sbs.ch>
2013-05-15 15:14:20 +0200

    There is no need to remove the file in the storage.
    Just return the name as is and this will ensure that the existing file
    will be overwritten.

--- documents/storage.py
+++ documents/storage.py
@@ -12,14 +12,4 @@ from django.core.files.storage import FileSystemStorage
 class OverwriteStorage(FileSystemStorage):
 
     def get_available_name(self, name):
-        """Returns a filename that's free on the target storage system, and
-        available for new content to be written to.
-
-        See http://stackoverflow.com/questions/9522759/imagefield-overwrite-image-file
-
-        This file storage overwrite existing files on upload.
-        """
-        # If the filename already exists, remove it
-        if self.exists(name):
-            self.delete(name)
         return name
