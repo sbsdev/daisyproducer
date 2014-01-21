26bf53562210608ce1f3bf8182d1d5ed061af9b3
Christian Egli <christian.egli@sbs.ch>
2014-01-21 15:11:05 +0100

    Simplify the error handling

--- documents/pipeline2/rest.py
+++ documents/pipeline2/rest.py
@@ -7,10 +7,7 @@ from urlparse import urlparse
 logger = logging.getLogger(__name__)
 
 def xml_from_string(xmlstr):
-    """Creates an XML document from a string"""
-    if xmlstr == None or xmlstr == "":
-        return None
-    return etree.fromstring(xmlstr)
+    return etree.fromstring(xmlstr) if xmlstr else None
 
 def send_request(uri, request_handler, response_handler, method, data=None):
     parseduri = urlparse(uri)
@@ -33,11 +30,8 @@ def get_resource(uri):
     def response_handler(response):
         if response.status == httplib.OK:
             return response.read()
-        elif response.status == httplib.NO_CONTENT:
-            return True
-        elif response.status == httplib.INTERNAL_SERVER_ERROR:
-            return None
         else:
+            logger.warn("Unexpected Server Response %s", response.status)
             return None
 
     return send_request(uri, request_handler, response_handler, "GET")
@@ -45,10 +39,7 @@ def get_resource(uri):
 def get_resource_as_xml(uri):
     """Return an XML document representing a resource"""
     resource = get_resource(uri)
-    if resource != None:
-        return xml_from_string(resource)
-    else:
-        return None
+    return xml_from_string(resource)
 
 def post_resource(uri, data):
     """Post a new resource"""
@@ -59,9 +50,8 @@ def post_resource(uri, data):
     def response_handler(response):
         if response.status == httplib.CREATED:
             return xml_from_string(response.read())
-        elif response.status == httplib.INTERNAL_SERVER_ERROR:
-            return None
         else:
+            logger.warn("Unexpected Server Response %s", response.status)
             return None
 
     return send_request(uri, request_handler, response_handler, "POST", data=data)
@@ -75,9 +65,8 @@ def delete_resource(uri):
     def response_handler(response):
         if response.status == httplib.NO_CONTENT:
             return True
-        elif response.status == httplib.INTERNAL_SERVER_ERROR:
-            return None
         else:
+            logger.warn("Unexpected Server Response %s", response.status)
             return None
 
     return send_request(uri, request_handler, response_handler, "DELETE")
