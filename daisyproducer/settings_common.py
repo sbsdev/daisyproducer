1f81e2e817a73703d6385e64d222885ffab5bf27
Christian Egli <christian.egli@sbs.ch>
2017-04-07 14:06:40 +0200

    Run the tests in sqlite db

--- daisyproducer/settings_common.py
+++ daisyproducer/settings_common.py
@@ -19,12 +19,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'daisyproducer_dev',     # Or path to database file if using sqlite3.
-        'USER': 'eglic',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'unittest.db',
     }
 }
 
--- daisyproducer/settings_unittest.py
+++ daisyproducer/settings_unittest.py
@@ -2,12 +2,6 @@ from settings_common import *
 
 DEBUG = TEMPLATE_DEBUG = False
 
-DATABASE_ENGINE = 'sqlite3'
-# FIXME: This should be changed to the new TEST setting
-# (https://docs.djangoproject.com/en/1.11/ref/settings/#std:setting-DATABASE-TEST).
-# Will be deprecated in Django 1.9
-TEST_DATABASE_NAME = 'unittest.db'
-
 DAISY_PIPELINE_PATH = os.path.join(PROJECT_DIR, '..', '..', 'tmp', 'pipeline')
 EXTERNAL_PATH = os.path.join(PROJECT_DIR, '..')
 
